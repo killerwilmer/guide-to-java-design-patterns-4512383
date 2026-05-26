49dc61fae7c3cae35ef7146f76891b25f18716d0
Wilmer Arteaga Gaitan <killerwilmer@gmail.com>
2026-05-26 14:30:36 +0000

    Refactor Person class to use Builder pattern for object creation

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -12,14 +12,13 @@ public class Person
     private final String emailAddress;
     private final String phoneNumber;
 
-    public Person(String firstName, String middleName, String lastName,
-                  LocalDate dateOfBirth, String emailAddress, String phoneNumber) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.emailAddress = emailAddress;
-        this.phoneNumber = phoneNumber;
+    public Person(PersonBuilder personBulider) {
+        this.firstName = personBulider.firstName;
+        this.middleName = personBulider.middleName;
+        this.lastName = personBulider.lastName;
+        this.dateOfBirth = personBulider.dateOfBirth;
+        this.emailAddress = personBulider.emailAddress;
+        this.phoneNumber = personBulider.phoneNumber;
         if (firstName == null || lastName == null) {
             throw new IllegalArgumentException("First name and last name must not be null");
         }
@@ -35,5 +34,43 @@ public class Person
                 ", emailAddress='" + emailAddress + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
+    }
+
+    public static class PersonBuilder {
+        private final String firstName;
+        private String middleName;
+        private final String lastName;
+        private LocalDate dateOfBirth;
+        private String emailAddress;
+        private String phoneNumber;
+
+        public PersonBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public PersonBuilder middleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public PersonBuilder dateOfBirth(LocalDate dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public PersonBuilder emailAddress(String emailAddress) {
+            this.emailAddress = emailAddress;
+            return this;
+        }
+
+        public PersonBuilder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
     }
 }
