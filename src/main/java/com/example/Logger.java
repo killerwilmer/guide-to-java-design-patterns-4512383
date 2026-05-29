cbd4ec63c92d81602a16564529656b07ed148847
Wilmer Arteaga Gaitan <killerwilmer@gmail.com>
2026-05-29 19:09:21 +0000

    Refactor Logger to use Singleton pattern and update Cat and Dog classes accordingly

--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -1,7 +1,12 @@
 package com.example;
 
-public class Logger {
+public enum Logger {
 
+    INSTANCE;
+
+    public static Logger getInstance() {
+        return INSTANCE;
+    }
 
     void log(String logMessage) {
         System.out.println(logMessage);
